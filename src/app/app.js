84f685d8f5d9632494f7ebeaee81b0758a5d5c25
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-07 18:38:53 +0100

    code

--- src/app/app.js
+++ src/app/app.js
@@ -2,18 +2,18 @@ const allSpan=document.querySelectorAll("span")
 const imageIphone=document.getElementById("phone")
 const header=document.querySelector('header')
 const cirle=document.querySelector('.circle')
-const box=document.querySelector('.box')
+const box=document.querySelector('.box');
+const url={
+    green:"./img/green.png",
+    pink:"./img/pink.png",
+    black:"./img/black.png",
+    blue:"./img/blue.png",
+    white:"./img/white.png",
+    red:"./img/red.png",
+ }
 
 const App=()=>{
-    console.log(box.style)
-    const listImage={
-        green:src=("../../img/green.png"),
-        pink:"../../img/pink.png",
-        blue:"../../img/blue.png",
-        black:"../../img/black.png",
-        white:"../../img/white.png",
-        red:"../../img/red.png",
-    }
+ 
     const changeHandleCor=(color,url)=>{
         return(
             header.style.background=color,
@@ -23,15 +23,14 @@ const App=()=>{
     }
     allSpan.forEach((element)=>{
         element.addEventListener('click', async() =>{
-            if(element.id==="green")await changeHandleCor("#384837",listImage.green);
-            if(element.id==="pink")await changeHandleCor("#f8ddd6",listImage.pink);
-            if(element.id==="blue")await changeHandleCor("#225f7e",listImage.blue);
-            if(element.id==="black")await changeHandleCor("#192028",listImage.black);
-            if(element.id==="white")await changeHandleCor("#f7f2ee",listImage.white)
-            if(element.id==="red")await changeHandleCor("#ae0617",listImage.red);
+            if(element.id==="green")await changeHandleCor("#384837",url.green);
+            if(element.id==="pink")await changeHandleCor("#f8ddd6",url.pink);
+            if(element.id==="blue")await changeHandleCor("#225f7e",url.blue);
+            if(element.id==="black")await changeHandleCor("#192028",url.black);
+            if(element.id==="white")await changeHandleCor("#f7f2ee",url.white)
+            if(element.id==="red")await changeHandleCor("#ae0617",url.red);
         })
     })
-    console.log(cirle.style.background)
 }
 
 App();
