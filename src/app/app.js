0a9724ad03450aedd9e17da105756c64df53b2cd
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-07 13:47:02 +0100

    add animations

--- src/app/app.js
+++ src/app/app.js
@@ -1,36 +1,33 @@
 const allSpan=document.querySelectorAll("span")
 const imageIphone=document.getElementById("phone")
+const header=document.querySelector('header')
 const cirle=document.querySelector('.circle')
-const listImage={
-    green:src=("../../img/green.png"),
-    pink:"../../img/pink.png",
-    blue:"../../img/blue.png",
-    black:"../../img/black.png",
-    white:"../../img/white.png",
-    red:"../../img/red.png",
-}
 
 const App=()=>{
-    const selectImage=()=>{
-        if(element.id==="green")imageIphone.src=listImage['green'];
-        if(element.id==="pink")imageIphone.src=listImage['pink'];
-        if(element.id==="blue")imageIphone.src=listImage['blue'];
-        if(element.id==="black")imageIphone.src=listImage['black'];
-        if(element.id==="white")imageIphone.src=listImage['white'];
-        if(element.id==="red")imageIphone.src=listImage['red'];
+    
+    const listImage={
+        green:src=("../../img/green.png"),
+        pink:"../../img/pink.png",
+        blue:"../../img/blue.png",
+        black:"../../img/black.png",
+        white:"../../img/white.png",
+        red:"../../img/red.png",
+    }
+    const changeHandleCor=(color,url)=>{
+        return(
+            cirle.style.background=color,
+            header.style.background=color,
+            imageIphone.src=url
+        )
     }
     allSpan.forEach((element)=>{
         element.addEventListener('click', async() =>{
-            if(element.id==="green"){
-                cirle.style.background="blue"
-               return imageIphone.src=listImage.green;
-
-            }
-            if(element.id==="pink")imageIphone.src=listImage.pink;
-            if(element.id==="blue")imageIphone.src=listImage['blue'];
-            if(element.id==="black")imageIphone.src=listImage['black'];
-            if(element.id==="white")imageIphone.src=listImage['white'];
-            if(element.id==="red")imageIphone.src=listImage['red'];
+            if(element.id==="green")changeHandleCor("#384837",listImage.green);
+            if(element.id==="pink")changeHandleCor("#f8ddd6",listImage.pink);
+            if(element.id==="blue")changeHandleCor("#225f7e",listImage.blue);
+            if(element.id==="black") changeHandleCor("#192028",listImage.black);
+            if(element.id==="white")changeHandleCor("#f7f2ee",listImage.white)
+            if(element.id==="red")changeHandleCor("#ae0617",listImage.red);
         })
     })
     console.log(cirle.style.background)
