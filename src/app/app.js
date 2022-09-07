99a375237cffe08a58e33a845b9993337fbc7a2f
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-07 13:16:39 +0100

    new functions

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,40 @@
-const spanElement=document.querySelectorAll("span")
-
-spanElement.forEach((id)=>{
-    console.log(id)
-})
-console.log()
-const imgeSlider=(event)=>{
-    document.querySelector("#phone").src=event
+const allSpan=document.querySelectorAll("span")
+const imageIphone=document.getElementById("phone")
+const cirle=document.querySelector('.circle')
+const listImage={
+    green:src=("../../img/green.png"),
+    pink:"../../img/pink.png",
+    blue:"../../img/blue.png",
+    black:"../../img/black.png",
+    white:"../../img/white.png",
+    red:"../../img/red.png",
 }
+
+const App=()=>{
+    const selectImage=()=>{
+        if(element.id==="green")imageIphone.src=listImage['green'];
+        if(element.id==="pink")imageIphone.src=listImage['pink'];
+        if(element.id==="blue")imageIphone.src=listImage['blue'];
+        if(element.id==="black")imageIphone.src=listImage['black'];
+        if(element.id==="white")imageIphone.src=listImage['white'];
+        if(element.id==="red")imageIphone.src=listImage['red'];
+    }
+    allSpan.forEach((element)=>{
+        element.addEventListener('click', async() =>{
+            if(element.id==="green"){
+                cirle.style.background="blue"
+               return imageIphone.src=listImage.green;
+
+            }
+            if(element.id==="pink")imageIphone.src=listImage.pink;
+            if(element.id==="blue")imageIphone.src=listImage['blue'];
+            if(element.id==="black")imageIphone.src=listImage['black'];
+            if(element.id==="white")imageIphone.src=listImage['white'];
+            if(element.id==="red")imageIphone.src=listImage['red'];
+        })
+    })
+    console.log(cirle.style.background)
+}
+
+App();
+
